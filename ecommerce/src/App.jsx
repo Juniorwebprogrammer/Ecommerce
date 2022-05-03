916fe29627098e0a85ea9fc9de259773c5261c55
JuniorWeb <juniorgarciadiaz@outlook.com>
2022-05-03 21:03:45 +0200

    Configuración src e inicio del desarrollo ( implementación de node-sass )

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,15 @@
+import "./app.scss";
+import { useState } from 'react';
+
+function App(){
+
+  return(
+    <div className="app">
+      <div className="sections">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
