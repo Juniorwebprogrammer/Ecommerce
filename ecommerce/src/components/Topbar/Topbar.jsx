b3ece507a791c2119b798a69267529b66e3b2767
Junior García <juniorgarciadiaz@outlook.com>
2022-05-09 23:16:05 +0200

    Squashed commit of the following:
    commit 05dd41a6de24763f2a5a45f8fcc211ba28f26d2f
    Author: Junior García <juniorgarciadiaz@outlook.com>
    Date:   Mon May 9 23:14:22 2022 +0200
    
        Retoque del componente Topbar

--- ecommerce/src/components/Topbar/Topbar.jsx
+++ ecommerce/src/components/Topbar/Topbar.jsx
@@ -1,33 +1,30 @@
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import('../../../public/image/logo.jpg');
 import ('./topbar.scss');
 
 
 export default function Topbar(){
     return(
     <div className='topbar'>
-        <div className="wrapper">
-            <div className="left">
-                <a href='#intro' className='logo'>
-                    HG La Fábrica en Casa
-                </a>
-                <div className="itemcontainer">
-                    <YouTubeIcon className='icon' onclick={()=>window.open("https://www.youtube.com/channel/UCb0zUX-LjRFm0bxXU4L-Kpw")}/>
-                    <span>Youtube Channel</span>
-                </div>
+       <div className="left">
+            <div className="logo">
+                
+            </div>
+            <div className='iconContainer'>
+               <div className="youtubeIcon">
 
-                <div className="itemcontainer">
-                <TwitterIcon className='icon' onclick={()=>window.open("https://www.youtube.com/channel/UCb0zUX-LjRFm0bxXU4L-Kpw")}/>
-                    <span>Youtube Channel</span>
-                </div>
+               </div>
+               <div className="socialIcon">
+                    
+               </div>
             </div>
-            
+       </div>
+       <div className="right">
             <div className="menu">
-                <span className='line-1'></span>
-                <span className='line-2'></span>
-                <span className='line-3'></span>
+
             </div>
-        </div>
+       </div>
     </div>
     );
 }
